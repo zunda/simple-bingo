502f1be8faaf93a7535668526b585fef6cdf8612
zunda <zundan@gmail.com>
2021-07-25 15:25:14 -1000

    Set cookie when bewing a card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -12,5 +12,6 @@ class CardsController < ApplicationController
 
   def show
     @card = Card.find(params[:id])
+    cookies.signed[:card_id] = { value: @card.id, expires: 1.week }
   end
 end
--- test/controllers/cards_controller_test.rb
+++ test/controllers/cards_controller_test.rb
@@ -9,4 +9,13 @@ class CardsControllerTest < ActionDispatch::IntegrationTest
     assert card_id, "Cookie for card ID is not set"
     assert_redirected_to card_path(card_id)
   end
+
+  test "showing a card sets a cookie" do
+    g = Game.create
+    c = Card.create(game: g)
+    get card_path(c.id)
+    jar = ActionDispatch::Cookies::CookieJar.build(request, cookies.to_hash)
+    card_id = jar.signed['card_id']
+    assert_equal c.id, card_id, "Cookie for card ID is not set"
+  end
 end
