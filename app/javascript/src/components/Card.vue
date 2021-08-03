03d4f334ecdf96002611f77b1df8b3c1e2fe8d5c
zunda <zundan@gmail.com>
2021-08-02 20:39:07 -1000

    Introduce Vue generated component into cards#show

---
+++ app/javascript/src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>
+    {{ message }}
+  </p>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'Card',
+  setup() {
+    const message = ref('ビンゴカード')
+    
+    return {
+      message
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  background-color: pink;
+}
+</style>
--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import Game from '@/components/Game.vue'
+import Card from '@/components/Card.vue'
 
 export default () => {
   document.addEventListener('DOMContentLoaded', () => {
     const gameApp = createApp(Game)
     gameApp.mount('#game-app')
+    const cardApp = createApp(Card)
+    cardApp.mount('#card-app')
   })
 }
