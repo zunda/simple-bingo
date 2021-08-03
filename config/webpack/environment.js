04c53559dd5a003ba43ebbaf779ecee9257c5472
zunda <zundan@gmail.com>
2021-08-02 20:17:51 -1000

    Configure WebPacker for Vue source

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,36 @@
 const { environment } = require('@rails/webpacker')
+const path = require("path")
+const { DefinePlugin } = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+
+const customConfig = {
+  resolve:{
+    alias: {
+      "@": path.resolve(__dirname, "..", "..", "app/javascript/src")
+    }
+  }
+}
+
+environment.config.merge(customConfig)
+
+environment.plugins.prepend(
+    'VueLoaderPlugin',
+    new VueLoaderPlugin()
+)
+
+environment.loaders.prepend('vue', {
+    test: /\.vue$/,
+    use: [{
+        loader: 'vue-loader'
+    }]
+})
+
+environment.plugins.prepend(
+    'Define',
+    new DefinePlugin({
+        __VUE_OPTIONS_API__: false,
+        __VUE_PROD_DEVTOOLS__: false
+    })
+)
 
 module.exports = environment
