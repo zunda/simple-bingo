ff6256096146526641458c50fa0fd39c8a0897af
zunda <zundan@gmail.com>
2021-08-07 16:29:54 -1000

    Only mount Vue app when necessary

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -4,9 +4,13 @@ import Card from '@/components/Card.vue'
 
 export default () => {
   document.addEventListener('DOMContentLoaded', () => {
-    const gameApp = createApp(Game)
-    gameApp.mount('#game-app')
-    const cardApp = createApp(Card)
-    cardApp.mount('#card-app')
+    if (document.getElementById('game-vue')) {
+      const gameApp = createApp(Game)
+      gameApp.mount('#game-vue')
+    }
+    if (document.getElementById('card-vue')) {
+      const cardApp = createApp(Card)
+      cardApp.mount('#card-vue')
+    }
   })
 }
