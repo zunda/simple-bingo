5587f4f7bf9b67f41352bba100d8c041f1a191b6
zunda <zundan@gmail.com>
2021-09-08 19:45:18 -1000

    Remove dependency to turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 // import "channels"
 // We will import channels from Vue components
@@ -12,5 +11,4 @@ import initVueApp from "@/main.js"
 
 initVueApp()
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
